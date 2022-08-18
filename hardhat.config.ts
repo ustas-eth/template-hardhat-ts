afa36b4f51157a3930077ebf397e865e4d15d160
ustas-eth <82833595+ustas-eth@users.noreply.github.com>
2022-08-18 08:40:04 +0000

    hardhat-deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "hardhat-deploy"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
-};
+}
 
 export default config;
