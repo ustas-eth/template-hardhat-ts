cb9cc5c4b8239072e1ab214c2c892edc0b99c653
OSvol <somethingtrash@riseup.net>
2022-10-11 15:44:56 +0100

    Updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,24 @@ import "@nomicfoundation/hardhat-toolbox"
 import "hardhat-deploy"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.16",
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.17',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
 }
 
 export default config;
